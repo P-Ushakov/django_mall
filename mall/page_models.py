939c9e6683929c4b80fc2ba2f6045b13ca9a28e4
Pavel Ushakov <pasha_ushakov@i.ua>
2019-02-06 22:34:05 +0200

    Star working on logic

--- mall/page_models.py
+++ mall/page_models.py
@@ -18,16 +18,24 @@ from taggit.models import TaggedItemBase
 
 # Mall object list
 class MlObjectIndexPage(Page):
-    intro = models.CharField(max_length=255,
-                             verbose_name='краткое описание',
+    intro = models.TextField(verbose_name='краткое описание',
                              blank=True)
+
+    # overriding default get_context to include only live objects, ordered by title
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        # can be ordered by published date: order_by('-first_published_at')
+        ml_objects = self.get_children().live().order_by('title')
+        context['ml_objects'] = ml_objects
+        return context
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro'),
+        FieldPanel('intro', classname="full"),
+        MultiFieldPanel([], heading='© Pavel Ushakov, BSD License'),
     ]
 
     class Meta:
@@ -65,24 +73,59 @@ class MlObjectTagIndexPage(Page):
 # Mall object
 class MlObjectPage(Page):
     buy_date = models.DateField("дата приобретения", blank=True, null=True)
-    intro = models.CharField(max_length=255,
-                             verbose_name='краткое описание',
+    intro = models.TextField(verbose_name='краткое описание',
                              blank=True)
     description = RichTextField(blank=True, verbose_name='описание')
     tags = ClusterTaggableManager(through=MlObjectTag, blank=True, verbose_name='ключи поиска')
 
+    # logical block
+    # is system normally operating
+    is_enabled = models.BooleanField(default=True, verbose_name='включен')
+    # is critical for parent system
+    is_critical = models.BooleanField(default=False, verbose_name='критически важен')
+    # is object periodically diagnosed
+    diagnosed = models.BooleanField(default=True, verbose_name='осмотрен')
+    # have normal maintenance
+    have_maintenance = models.BooleanField(default=True, verbose_name='обслужен')
+    # object have cll-down to it's work
+    call_down = models.BooleanField(default=False, verbose_name='есть замечания')
+    # system working, but should be repaired
+    have_to_be_repaired = models.BooleanField(default=False, verbose_name='требует ремонта')
+    # if critical and critically broken, then parent system broken too
+    is_critically_broken = models.BooleanField(default=False, verbose_name='сломан')
+    # automatic fields
+    broken_parts_count = models.IntegerField(default=0, verbose_name='некритичных повреждений')
+    critically_broken_parts_count = models.IntegerField(default=0, verbose_name='критичных повреждений')
+
+    # search block
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('description'),
     ]
 
+    # control panels
     content_panels = Page.content_panels + [
         MultiFieldPanel([
-            FieldPanel('buy_date'),
-            FieldPanel('tags'),
-        ], heading="Ключи поиска"),
-        FieldPanel('intro'),
+            FieldPanel('is_enabled'),
+            FieldPanel('is_critical'),
+            FieldPanel('diagnosed'),
+            FieldPanel('have_maintenance'),
+            FieldPanel('call_down'),
+            FieldPanel('have_to_be_repaired'),
+            FieldPanel('is_critically_broken'),
+
+        ], heading="Состояние объекта",
+            classname="collapsible collapsed"),
+        FieldPanel('tags'),
+        FieldPanel('intro', classname="full"),
         FieldPanel('description', classname="full"),
+        MultiFieldPanel([], heading='© Pavel Ushakov, BSD License')
+    ]
+
+    settings_panels = Page.settings_panels + [
+        FieldPanel('buy_date'),
+        FieldPanel('broken_parts_count'),
+        FieldPanel('critically_broken_parts_count'),
     ]
 
     class Meta:
