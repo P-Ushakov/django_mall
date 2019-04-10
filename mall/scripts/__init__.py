f733ab5a010de0764e4bab513019f5d5f8711bba
Pavel Ushakov <pasha_ushakov@i.ua>
2019-04-10 18:46:56 +0300

    experementing with menu

---
+++ mall/scripts/__init__.py
@@ -0,0 +1,1 @@
+from mall.scripts.actions import *
---
+++ mall/scripts/actions.py
@@ -0,0 +1,15 @@
+
+# shold be done in model
+def btn_action():
+    pass
+
+class MlButton:
+    def __init__(self, symbol, name, alert, position, btn_action):
+        self.symbol = symbol
+        self.name = name
+        self.alert = alert
+        self.position = position
+        self.btn_action = btn_action
+
+    def add_submenu_item(self, item):
+        pass
